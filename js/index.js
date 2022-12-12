ddc264864fa527ba33ebcc94fe26745e59e78a6d
Muhammad Shaheer <shaheer@uptek.com>
2022-12-12 23:34:36 +0500

    desktop version complete

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
+"use strict";
+
 let buttons = document.querySelectorAll(".slider__button button");
 let slides = document.querySelectorAll(".slider__wrapper-slide");
-
+// let btnOverlay = document.querySelector(".slider__button::before");
 const removeActiveClass = function (childElems, slideElems) {
   for (let i = 0; i < childElems.length; i++) {
+    console.log(childElems);
     childElems[i].classList.remove("active");
     slideElems[i].classList.remove("active");
   }
@@ -14,7 +17,45 @@ buttons.forEach((button, i) => {
       this.parentElement.children,
       slides[i].parentElement.children
     );
+
     button.classList.add("active");
     slides[i].classList.add("active");
+
+    // let btnStyles = window.getComputedStyle(button);
+    // let btnWidth = parseInt(btnStyles.getPropertyValue("width"));
+
+    //   btnOverlay.style.cssText = `left: ${
+    //     button.getBoundingClientRect().left
+    //   }px; width: ${btnWidth}px `;
   });
 });
+
+let counterSection = document.querySelector(".meta-data");
+let counterEL = document.querySelectorAll(".meta-data__data span");
+
+counterEL.forEach((counter) => {
+  let valueLength = counter.getAttribute("data-value").length;
+  counter.style.cssText = `width: ${valueLength * 16}px`;
+});
+
+const options = {
+  root: null,
+  threshold: 0,
+};
+
+const sectionObserver = new IntersectionObserver(function (entries) {
+  entries.forEach((entry) => {
+    counterEL.forEach((item) => {
+      item.getAttribute("data-value");
+      anime({
+        targets: item,
+        innerHTML: ["0000", item.getAttribute("data-value")],
+        round: 1,
+        duration: 2000,
+      });
+      // observer.unobserve(counterSection);
+    });
+  });
+}, options);
+
+sectionObserver.observe(counterSection);
