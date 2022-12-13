61a9abe3c39682d64e6fc4ebd83112b96e59430f
Muhammad Shaheer <shaheer@uptek.com>
2022-12-13 22:13:42 +0500

    mobile version complete

--- js/index.js
+++ js/index.js
@@ -1,34 +1,25 @@
-let buttons = document.querySelectorAll(".slider__button button");
+let sliderNavigation = document.querySelectorAll(".slider__button button");
 let slides = document.querySelectorAll(".slider__wrapper-slide");
+let counterSection = document.querySelector(".meta-data");
+let counterEL = document.querySelectorAll(".meta-data__data span");
+
 const removeActiveClass = function (childElems, slideElems) {
   for (let i = 0; i < childElems.length; i++) {
-    console.log(childElems);
     childElems[i].classList.remove("active");
     slideElems[i].classList.remove("active");
   }
 };
 
-buttons.forEach((button, i) => {
+sliderNavigation.forEach((button, i) => {
   button.addEventListener("click", function () {
     removeActiveClass(
       this.parentElement.children,
       slides[i].parentElement.children
     );
-
     button.classList.add("active");
     slides[i].classList.add("active");
-
-    // let btnStyles = window.getComputedStyle(button);
-    // let btnWidth = parseInt(btnStyles.getPropertyValue("width"));
-
-    //   btnOverlay.style.cssText = `left: ${
-    //     button.getBoundingClientRect().left
-    //   }px; width: ${btnWidth}px `;
   });
 });
-
-let counterSection = document.querySelector(".meta-data");
-let counterEL = document.querySelectorAll(".meta-data__data span");
 
 counterEL.forEach((counter) => {
   let valueLength = counter.getAttribute("data-value").length;
@@ -42,16 +33,20 @@ const options = {
 
 const sectionObserver = new IntersectionObserver(function (entries, observer) {
   entries.forEach((entry) => {
-    counterEL.forEach((item) => {
-      item.getAttribute("data-value");
-      anime({
-        targets: item,
-        innerHTML: ["0000", item.getAttribute("data-value")],
-        round: 1,
-        duration: 2000,
+    if (entry.isIntersecting) {
+      counterEL.forEach((item) => {
+        item.getAttribute("data-value");
+        anime({
+          targets: item,
+          innerHTML: ["0000", item.getAttribute("data-value")],
+          round: 1,
+          duration: 2000,
+        });
       });
       observer.unobserve(entry.target);
-    });
+    } else {
+      return;
+    }
   });
 }, options);
 
